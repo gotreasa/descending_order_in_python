98e60244cefa49b1002f9323f1dc16f936096d62
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-02-01 14:34:56 +0000

    refactor: programmatically calculate the number in descending order

--- docs/NOTES.md
+++ docs/NOTES.md
@@ -14,5 +14,5 @@
 - ✅ should return 1 when input is 1
 - ✅ should return 54421 when the input is 42145
 - ✅ should return 654321 when the input is 145263
-- 🚧 should return 987654321 when the input is 123456789
+- ✅ should return 987654321 when the input is 123456789
 - ⚠ should error if not a positive number
--- modules/descending_order.py
+++ modules/descending_order.py
@@ -1,10 +1,8 @@
 def descending_order(number: int) -> int:
-    if number == 1:
-        return 1
-    if number == 42145:
-        return 54421
-    if number == 145263:
-        return 654321
-    if number == 123456789:
-        return 987654321
-    raise ValueError("❗️ Input should be a number")
+    if not isinstance(number, int):
+        raise ValueError("❗️ Input should be a number")
+
+    numbers = str(number)
+    sorted_numbers = sorted([*numbers], reverse=True)
+    full_number = "".join(sorted_numbers)
+    return int(full_number)
